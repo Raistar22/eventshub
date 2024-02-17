750455beff6398c94a2bdd76629867e9db37c3a9
Or-10 <omraj132002@gmail.com>
2024-02-17 16:16:11 +0530

    second commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -5,40 +5,36 @@ import { Button } from "../ui/button"
 import NavItems from "./NavItems"
 import MobileNav from "./MobileNav"
 
-
 const Header = () => {
   return (
-    <header className="w-full h-16 border-b">
+    <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-      <Link href="/" className="w-36">
-        <Image src="/assets/images/logo.svg" width={128} height={38}
-        alt="EventsHub Logo"/>
-      </Link>
-      
-      <SignedIn>
-          <nav className="md:flex-between hidden w-full max-w-xs"> 
-          <NavItems />
-          </nav>
-        
-      </SignedIn>
+        <Link href="/" className="w-36">
+          <Image 
+            src="/assets/images/logo.svg" width={128} height={38}
+            alt="Evently logo" 
+          />
+        </Link>
 
-      <div className="flex w-32 justify-end gap 3">
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
-          <MobileNav />
-         
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
         </SignedIn>
 
-
-
-        <SignedOut>
-          <Button asChild className="rounded-full" size="lg">
-            <Link href="/sign-in">
-              Login
-            </Link>
-          </Button>
-        </SignedOut>
-      </div>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   )
