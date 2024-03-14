20705071888d70e82d21bec86d51f26d3b2fc428
Or-10 <omraj132002@gmail.com>
2024-03-14 21:25:05 +0530

    edited nav bar

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,7 +7,7 @@ import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
-    <header className="w-full border-b opacity-0.5">
+    <header className="w-full border-b border-gray-200 bg-white bg-opacity-25 backdrop-blur-lg backdrop-filter backdrop-saturate-150">
       <div className="wrapper flex items-center justify-between p-0">
         <Link href="/" className="w-36">
           <Image 
